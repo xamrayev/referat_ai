38599e694c7c1f8641914356e6e2d7746bace8f2
xamrayev <142881269+xamrayev@users.noreply.github.com>
2024-10-23 11:29:08 +0500

    Update telegram_api.py

--- telegram_api.py
+++ telegram_api.py
@@ -9,30 +9,39 @@ config = dotenv_values(".env")
 
 bot = telebot.TeleBot(config["TELEGRAM_API"])
 
-
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
 	user_id = message.from_user.id  # Get the user's ID
-	bot.reply_to(message, f"Your user ID is {user_id}")
-
-	# bot.replyto(message, "Referat mavzusini kiriting")
-
-@bot.message_handler(commands=['yarat'])
-def doc_yarat(message):
-	user_id = message.from_user.id
-	new_user(user_input=user_id)
-	bot.reply_to(message, "Sizga 5 ta limit ajratildi")
+	user_name = message.from_user.username  # Get the user's name
+	bot.send_message(message.chat.id, 
+							f"""Assalomu alaykum {user_name}. \n \tXush kelibsiz! 
+							🧞 \n \tSizga ✋ ta limit ajratildi.""")
 
 
-@bot.message_handler(func=lambda message: True)
+
+# @bot.message_handler(commands=['yarat'])
+# def doc_yarat(message):
+# 	user_id = message.from_user.id
+# 	new_user(user_input=user_id)
+# 	bot.reply_to(message, "Sizga 5 ta limit ajratildi")
+
+
+@bot.message_handler(commands=["mavzu"])
+def mavzu(message):
+	msg = bot.send_message(message.chat.id, "Referat mavzusini kiriting")
+	bot.register_next_step_handler(msg, echo_all)
+
 def echo_all(message):
 	referat_mavzusi=message.text
 	referat_izoh = gpt_surov(referat_mavzusi)
 	print("1-bosqich")
+	msg = bot.send_message(message.chat.id, "1-bosqich")
 	doc_creator(referat_mavzusi, referat_izoh)
 	doc=open("./generated_doc.docx", "rb")
 	print("2-bosqich")
+	msg = bot.send_message(message.chat.id, "2-bosqich")
 	bot.send_document(message.chat.id, doc)
 	print("tugadi")
 
+print("Bot ishga tushdi! \n")
 bot.infinity_polling()
